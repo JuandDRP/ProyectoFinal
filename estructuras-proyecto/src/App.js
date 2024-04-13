53058d73aeff031aa9fb72829c802531d9c4f39a
MiguelAngeloTr <miguelmax248@gmail.com>
2024-04-12 23:14:40 -0500

    Corrigiendo error

--- estructuras-proyecto/src/App.js
+++ estructuras-proyecto/src/App.js
@@ -1,18 +1,24 @@
+import React from 'react';
 import './App.css';
 import Pelicula from './componentes/Pelicula.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Utilizamos Routes en lugar de Route
+
+import PaginaPrincipal from './componentes/PaginaPrincipal.js';
+import Comidas from './componentes/Comidas.js';
+import SobreNosotros from './componentes/SobreNosotros.js';
 
 function App() {
   return (
     <div className="App">
-      <div className="contenedor-principal">
-        <h1>
-        <Pelicula />
-          {/* <Pelicula  pelicula="KunfuPanda" genero={1}/>
-          <Pelicula pelicula="ElExorcista" genero={2}/> */}
-        </h1>
-
-
-      </div>
+    
+      <Router>
+        <Routes> {/* Envuelve tus Routes dentro del componente Routes */}
+          <Route path="/" element={<PaginaPrincipal />} />
+          <Route path="/peliculas"  element={<Pelicula /> } />
+          <Route path="/comidas"  element={<Comidas /> } />
+          <Route path="/sobre-nosotros"  element={<SobreNosotros /> } />
+        </Routes>
+      </Router>
     </div>
   );
 }
