de3819539c202093265235aeb15e8fa3d165593c
juandDRP <juandrp2004@gmail.com>
2024-04-21 23:35:45 -0500

    Se añade implementacion de login y registro con firebase

--- estructuras-proyecto/src/App.js
+++ estructuras-proyecto/src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
 import './App.css';
 import Pelicula from './componentes/Pelicula.js';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Utilizamos Routes en lugar de Route
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Utilizamos Routes en lugar de Route
 import PaginaPrincipal from './componentes/PaginaPrincipal.js';
 import Comidas from './componentes/Comidas.js';
 import SobreNosotros from './componentes/SobreNosotros.js';
+import appfirebase from './credenciales.js';
+import {getAuth,onAuthStateChanged} from 'firebase/auth';
+const auth=getAuth(appfirebase)
+
 
 function App() {
+  const [usuario,setUsuario]=useState(null)
+  onAuthStateChanged(auth,(usuarioFirebase)=>{
+    if(usuarioFirebase){
+      setUsuario(usuarioFirebase)
+    }
+    else{
+      setUsuario(null)
+    }
+  })
+  
   return (
-    <div className="App">
-    
-      <Router>
-        <Routes> {/* Envuelve tus Routes dentro del componente Routes */}
-          <Route path="/" element={<PaginaPrincipal />} />
-          <Route path="/peliculas"  element={<Pelicula /> } />
-          <Route path="/comidas"  element={<Comidas /> } />
-          <Route path="/sobre-nosotros"  element={<SobreNosotros /> } />
-        </Routes>
-      </Router>
+    <div>
+      {usuario ? (
+        <Router>
+          <Routes>
+            <Route path="/" element={<Pelicula />} />
+            <Route path="/comidas" element={<Comidas />} />
+            <Route path="/sobre-nosotros" element={<SobreNosotros />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Router>
+      ) : (
+        <PaginaPrincipal />
+      )}
     </div>
+    // <div>
+    //   {usuario ? <Pelicula/>:<PaginaPrincipal />}
+    // </div>
+    // <div className="App">
+    //   <Router>
+    //     <Routes> {/* Envuelve tus Routes dentro del componente Routes */}
+    //       <Route path="/" element={<PaginaPrincipal />} />
+    //       <Route path="/peliculas"  element={<Pelicula /> } />
+    //       <Route path="/comidas"  element={<Comidas /> } />
+    //       <Route path="/sobre-nosotros"  element={<SobreNosotros /> } />
+    //     </Routes>
+    //   </Router>
+    // </div>
   );
 }
 
--- estructuras-proyecto/src/componentes/PaginaPrincipal.js
+++ estructuras-proyecto/src/componentes/PaginaPrincipal.js
@@ -1,59 +1,84 @@
-import imagen from "./GatoLogin.jpg"
-import React, { useState } from 'react';
+/* eslint-disable no-unused-vars */
+import imagen from "./GatoLogin.jpg";
+import React, { useState } from "react";
+import appfirebase from "../credenciales";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
+const auth = getAuth(appfirebase);
 function PaginaPrincipal({ peliculas }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
+  const [registrando, setRegistrado] = useState(false);
+  const functAutenticacion = async (e) => {
     e.preventDefault();
-    // Aquí puedes implementar la lógica de autenticación, por ejemplo, enviar los datos al servidor
-    console.log('Email:', email);
-    console.log('Password:', password);
+    const correo = e.target.email.value;
+    const contraseña = e.target.password.value;
+
+    if (registrando) {
+      try {
+        await createUserWithEmailAndPassword(auth, correo, contraseña);
+      } catch (error) {
+        alert("La contraseña debe ser mayor a 8 caracteres")
+      }
+    } else {
+      try {
+        await signInWithEmailAndPassword(auth, correo, contraseña);
+      } catch (error) {
+        alert("El correo o la contraseña son incorrectos");
+      }
+    }
   };
   return (
     <div className="bg-black flex justify-center items-center h-screen flex-col">
-      <form className="contenedor-principal flex flex-row h-50" onSubmit={handleSubmit}>
+      <form
+        onSubmit={functAutenticacion}
+        className="contenedor-principal flex flex-row h-50"
+      >
         <div className="formulario w-50">
           <h1 className="text-5xl mt-5 mb-9 uppercase text-red-700">Login</h1>
           <p className="text-red-700 text-4xl text-left">Email</p>
           <input
             className="input-email border-black text-4xl mb-4"
             type="email"
-            value={email}
-            onChange={handleEmailChange}
+            // value={email}
+            // onChange={handleEmailChange}
             required
+            id="email"
           />
           <p className="text-red-700 text-4xl text-left">Password</p>
           <input
             className="input-password border text-4xl mb-4"
             type="password"
-            value={password}
-            onChange={handlePasswordChange}
             required
+            id="password"
           />
           <p className="text-blue-600 text-10 text-left">ForgotPassword?</p>
+          <div className="botones flex justify-between items my-6">
+            <button className="bg-red-700 text-white px-4 py-2 rounded">
+              {registrando ? "Registrate" : "Iniciar sesion"}
+            </button>
+          </div>
         </div>
         <div className="contenedor-imagen my-6">
           <img className="w-80 m-auto" src={imagen} alt="Gato del login" />
         </div>
       </form>
-      <div className="botones flex justify-between items w-1/3 my-6">
-        <button className="bg-red-700 text-white px-4 py-2 rounded" type="submit">Login</button>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">Register</button>
+
+      <div className="botones flex justify-between my-6">
+        <h1 className="text-blue-600 text-10 text-left">
+          {registrando ? "Si ya tienes cuenta" : "No tienes cuenta"}
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+            onClick={() => setRegistrado(!registrando)}
+          >
+            {registrando ? "Iniciar sesion" : "Registrate"}
+          </button>
+        </h1>
       </div>
     </div>
   );
 }
-
-
 
 export default PaginaPrincipal;
