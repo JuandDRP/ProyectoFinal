0ea320db87e0ff0126ef5570e98add13534db224
Juan de Dios Rodriguez <135068211+JuandDRP@users.noreply.github.com>
2024-04-22 00:16:23 -0500

    Revert "Prueba"

--- estructuras-proyecto/src/componentes/Navbar.js
+++ estructuras-proyecto/src/componentes/Navbar.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import "../hojas-de-estilo/Navbar.css";
 import { useState } from "react";
@@ -127,23 +126,6 @@ export const Navbar = ({
             </div>
           </div>
         </li>
-
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import '../hojas-de-estilo/Navbar.css'
-import appfirebase from './../credenciales'
-import {getAuth,signOut} from 'firebase/auth';
-const auth=getAuth(appfirebase);
-function Navbar() {
-  
-  return (
-    <nav>
-      <ul>
-        <li><a href="/peliculas">Cartelera</a></li>
-        <li><a href="/comidas">Comidas</a></li>
-        <li><a href="/sobre-nosotros">Sobre Nosotros</a></li>
-        <button className='btn' onClick={()=>signOut(auth)}>Cerrar sesion</button>
-
       </ul>
     </nav>
   );
