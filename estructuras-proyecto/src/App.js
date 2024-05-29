3a63f176f53caaeb3a95d51f2a350b3c06fcd646
juandDRP <juandrp2004@gmail.com>
2024-05-28 23:58:11 -0500

    Cambios

--- estructuras-proyecto/src/App.js
+++ estructuras-proyecto/src/App.js
@@ -105,7 +105,6 @@ function App() {
                 </>
               }
             />
-            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </Router>
       ) : (
